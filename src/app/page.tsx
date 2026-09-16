0e932ace0f2a97f605bebe32e0aa127dbf310471
mmei203 <meisyandaryani@gmail.com>
2026-09-16 11:55:53 +0800

    wireframe landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,69 +1,147 @@
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert h-5 w-[100px]"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the{" "}
-            <code className="rounded bg-black/[.06] px-1.5 py-0.5 font-mono text-[0.9em] dark:bg-white/[.08]">
-              page.tsx
-            </code>{" "}
-            file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <main className="">
+      {/* HERO SECTION */}
+      <div className="p-10 container mx-auto flex lg:flex-row gap-4 flex-col min-h-screen min-w-screen border-t border-black">
+        <div className="lg:text-8xl text-5xl font-bold basis-3/3 flex flex-col">
+          <h1>Lorem, ipsum,</h1>
+          <h1>ATMINT JOMOK</h1>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert h-[14px] w-4"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={14}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="basis-1/2 flex flex-col justify-center gap-4">
+          <div className="px-2">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, ad nam nihil incidunt non voluptatem neque quisquam praesentium maxime velit nostrum inventore odio ex consectetur ducimus totam maiores exercitationem pariatur.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi, ad nam nihil incidunt non voluptatem neque quisquam praesentium maxime velit nostrum inventore odio ex consectetur ducimus totam maiores exercitationem pariatur.</p>
+          </div>
+          <div className="flex flex-col">
+            <Link href='/'>
+              <Button variant="ghost" className="py-6 text-xl lg:text-2xl font-bold flex items-center rounded-md hover:bg-primary hover:text-white">Lihat Project <ArrowRight className="size-5"/></Button>
+            </Link>
+            <Link href='/'>
+              <Button variant="ghost" className="py-6 text-xl lg:text-2xl font-bold flex items-center rounded-md hover:bg-primary hover:text-white">Hubungi Saya <ArrowRight className="size-5"/></Button>
+            </Link>
+          </div>
         </div>
-      </main>
-    </div>
+      </div>
+
+      {/* ABOUT SECTION */}
+      <div className="p-5 lg:p-20 mx-auto flex flex-col lg:flex-row min-h-screen border-t border-black">
+        <div className="flex basis-4/3 invisible lg:visible">
+          <h1 className="lg:text-9xl font-bold">01</h1>
+        </div>
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-5xl font-bold">Tentang Saya</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ab dolore optio soluta et atque facere totam aperiam possimus eos beatae, id assumenda magni vero unde, sit vitae ipsam velit?</p>
+          </div>
+          <div className="grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-0">
+            <div className="border-black border">
+              <Image src="https://placehold.co/600x400/png" alt="image placeholder" width="400" height="300"/>
+              <div className="p-5">
+                <h2 className="text-2xl font-bold">Foto & Biografi</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum dignissimos quod suscipit unde voluptatem, similique in adipisci nobis necessitatibus porro!</p>
+              </div>
+            </div>
+            <div className="border-black border">
+              <div className="p-5">
+                <h2 className="text-2xl font-bold">Skills</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum dignissimos quod suscipit unde voluptatem, similique in adipisci nobis necessitatibus porro!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* PROJECT SECTION */}
+      <div className="p-5 lg:p-20 mx-auto flex flex-col lg:flex-row min-h-screen border-t border-black">
+        <div className="flex basis-4/3 invisible lg:visible">
+          <h1 className="lg:text-9xl font-bold">02</h1>
+        </div>
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-5xl font-bold">Project Saya</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ab dolore optio soluta et atque facere totam aperiam possimus eos beatae, id assumenda magni vero unde, sit vitae ipsam velit?</p>
+          </div>
+          <div className="grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-0">
+            <div className="border-black border">
+              <Image src="https://placehold.co/600x400/png" alt="image placeholder" width="400" height="300"/>
+              <div className="p-5">
+                <h2 className="text-2xl font-bold">Foto & Biografi</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum dignissimos quod suscipit unde voluptatem, similique in adipisci nobis necessitatibus porro!</p>
+              </div>
+            </div>
+            <div className="border-black border">
+              <Image src="https://placehold.co/600x400/png" alt="image placeholder" width="400" height="300"/>
+              <div className="p-5">
+                <h2 className="text-2xl font-bold">Skills</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum dignissimos quod suscipit unde voluptatem, similique in adipisci nobis necessitatibus porro!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* EXPERIENCE SECTION */}
+      <div className="p-5 lg:p-20 mx-auto flex flex-col lg:flex-row min-h-screen border-t border-black">
+        <div className="flex basis-4/3 invisible lg:visible">
+          <h1 className="lg:text-9xl font-bold">03</h1>
+        </div>
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-5xl font-bold">Pengalaman</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ab dolore optio soluta et atque facere totam aperiam possimus eos beatae, id assumenda magni vero unde, sit vitae ipsam velit?</p>
+          </div>
+          <div className="grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-0">
+            <div className="border-black border">
+              <Image src="https://placehold.co/600x400/png" alt="image placeholder" width="400" height="300"/>
+              <div className="p-5">
+                <h2 className="text-2xl font-bold">Foto & Biografi</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum dignissimos quod suscipit unde voluptatem, similique in adipisci nobis necessitatibus porro!</p>
+              </div>
+            </div>
+            <div className="border-black border">
+              <Image src="https://placehold.co/600x400/png" alt="image placeholder" width="400" height="300"/>
+              <div className="p-5">
+                <h2 className="text-2xl font-bold">Skills</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum dignissimos quod suscipit unde voluptatem, similique in adipisci nobis necessitatibus porro!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* CONTACT SECTION */}
+      <div className="p-5 lg:p-20 mx-auto flex flex-col lg:flex-row min-h-screen border-t border-black">
+        <div className="flex basis-4/3 invisible lg:visible">
+          <h1 className="lg:text-9xl font-bold">04</h1>
+        </div>
+        <div className="flex flex-col gap-10">
+          <div className="flex flex-col gap-4">
+            <h1 className="text-5xl font-bold">Hubungi Saya</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi ab dolore optio soluta et atque facere totam aperiam possimus eos beatae, id assumenda magni vero unde, sit vitae ipsam velit?</p>
+          </div>
+          <div className="grid grid-cols-1 grid-rows-2 lg:grid-cols-2 lg:grid-rows-1 gap-0">
+            <div className="border-black border">
+              <Image src="https://placehold.co/600x400/png" alt="image placeholder" width="400" height="300"/>
+              <div className="p-5">
+                <h2 className="text-2xl font-bold">Foto & Biografi</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum dignissimos quod suscipit unde voluptatem, similique in adipisci nobis necessitatibus porro!</p>
+              </div>
+            </div>
+            <div className="border-black border">
+              <Image src="https://placehold.co/600x400/png" alt="image placeholder" width="400" height="300"/>
+              <div className="p-5">
+                <h2 className="text-2xl font-bold">Skills</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Harum dignissimos quod suscipit unde voluptatem, similique in adipisci nobis necessitatibus porro!</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
